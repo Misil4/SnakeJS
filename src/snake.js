5c2984edea28169539074e3d383ae6e3a9fcef3e
Misil <carlossilvamarques.cs@gmail.com>
2021-09-24 09:08:51 +0200

    (UPDATE) solved bug when colising with the fruit

--- src/snake.js
+++ src/snake.js
@@ -8,20 +8,26 @@ const styles = {
 }
 let snake = [{
 x: 200,y :200}]
+let counter = 0;
 
 let changingDirection = false;
+let food_x;
+let food_y;
 let dx = 10;
 let dy = 0;
+const counter_div = document.getElementById('counter');
 const board = document.getElementById('snakeboard');
 const ctx = board.getContext('2d');
 start()
+gen_food();
 document.addEventListener("keydown",snake_direction);
 
 function start() {
-    if (has_game_ended()) return;
+    if (has_game_ended()){alert('YOU LOSE');document.reload()};
     changingDirection = false;
     setTimeout(function onTick() {
         drawBoard();
+        drawFood();
         snake_move();
         drawSnake();
         start();
@@ -30,7 +36,7 @@ function start() {
 
 function drawSnakeParts(part) {
     ctx.fillStyle = styles.snake_col;
-    ctx.Strokestyle = styles.snake_border;
+    ctx.strokestyle = styles.snake_border;
     ctx.fillRect(part.x, part.y, 10, 10);
     ctx.strokeRect(part.x, part.y, 10, 10)
 }
@@ -41,7 +47,7 @@ function drawSnake() {
 
 function drawBoard() {
     ctx.fillStyle = styles.board_background;
-    ctx.Strokestyle = styles.board_border;
+    ctx.strokestyle = styles.board_border;
     ctx.fillRect(0, 0, board.width, board.height);
     ctx.strokeRect(0, 0, board.width, board.height);
 }
@@ -52,6 +58,14 @@ function snake_move() {
         y: snake[0].y + dy
     };
     snake.unshift(snake_head)
+    const has_eaten_food = snake[0].x === food_x && snake[0].y === food_y;
+    if (has_eaten_food) {
+    gen_food();
+    counter++;
+    counter_div.innerHTML = counter;
+    snake.push({x: 200, y: snake[snake.length-1].y-10});
+
+    }
     snake.pop();
 }
 function has_game_ended() {
@@ -60,6 +74,23 @@ function has_game_ended() {
     const hitUpWall = snake[0].y <0;
     const hitDownWall = snake[0].y > board.width -10;
     return hitLeftWall || hitRightWall || hitUpWall || hitDownWall;
+}
+function drawFood() {
+  ctx.fillStyle = 'lightgreen';
+  ctx.strokestyle = 'darkgreen';
+  ctx.fillRect(food_x, food_y,10,10);
+  ctx.strokeRect(food_x, food_y,10,10);
+}
+function random_food(min, max) {
+  return Math.round((Math.random() * (max-min) + min) / 10) * 10;
+}
+function gen_food() {
+  food_x = random_food(0,snakeboard.width -10);
+  food_y = random_food(0,snakeboard.height -10);
+  snake.forEach(function has_snake_eaten_food(part) {
+    const has_eaten = part.x == food_x && part.y == food_y;
+    if (has_eaten) gen_food();
+  });
 }
 function snake_direction(event) {
     const LEFT_KEY = 37;
